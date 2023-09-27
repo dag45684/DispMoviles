f171b897deb25cb9d1c04ae4b27c1e6a1c722abf
dag45684 <dag45684@educastur.es>
2023-09-27 20:58:49 +0200

    little thingies im bored

--- Contador/app/src/main/java/com/example/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/contador/MainActivity.java
@@ -6,14 +6,19 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView counter;
     TextView mikusais;
+    LinearLayout lup;
     Button b;
+    TextView t3;
     int addition = 1;
+    int level = 1;
+    int costemejora = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +27,20 @@ public class MainActivity extends AppCompatActivity {
         counter = (TextView) findViewById(R.id.countertext);
         b = (Button) findViewById(R.id.countersum);
         mikusais = (TextView) findViewById(R.id.mikusay);
+        t3 = (TextView) findViewById(R.id.t3);
+        lup = (LinearLayout) findViewById(R.id.lup);
     }
 
     public void sum(View v){
         counter.setTextColor(Color.rgb(0,0,0));
         int n = Integer.parseInt(counter.getText().toString()) + addition;
         counter.setText(Integer.toString(n));
-        if(n >= 120){
-//           b.setEnabled(false);
-//           counter.setText("Hurry and level up!");
-//           counter.setTextSize(20);
+        if(n < costemejora){
+            lup.setEnabled(false);
+            lup.setBackgroundColor(Color.rgb(220,220,220));
         }else{
+            lup.setEnabled(true);
+            lup.setBackgroundColor(Color.rgb(255,200,180));
             for (int i=2; i<=((int)n/2)+1; i++){
                 if (n%i == 0 && n!=2){
                     counter.setTextColor(Color.rgb(0,0,0));
@@ -43,19 +51,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void levelup(View v){
-        if(Integer.parseInt(counter.getText().toString()) >= 100){
-            b.setEnabled(true);
-            counter.setTextColor(Color.rgb(0,0,0));
-            counter.setText("" + (Integer.parseInt(counter.getText().toString()) - 100));
-            addition += 1;
-            mikusais.setText("Tap me to upgrade \n Current level: " + Integer.toString(addition-1));
-            b.setText("+" + Integer.toString(addition));
+        if(level <= 3){
+            if(Integer.parseInt(counter.getText().toString()) >= costemejora){
+                level++;
+                b.setEnabled(true);
+                counter.setTextColor(Color.rgb(0,0,0));
+                counter.setText("" + (Integer.parseInt(counter.getText().toString()) - costemejora));
+                addition += 1;
+                mikusais.setText("Tap me to upgrade \n Current level: " + Integer.toString(addition-1));
+                b.setText("+" + Integer.toString(addition));
+            }
+        }else if (level <= 5){
+            costemejora=200;
+            if(Integer.parseInt(counter.getText().toString()) >= costemejora) {
+                level++;
+                b.setEnabled(true);
+                counter.setTextColor(Color.rgb(0, 0, 0));
+                counter.setText("" + (Integer.parseInt(counter.getText().toString()) - costemejora));
+                addition *= 2;
+                mikusais.setText("Tap me to upgrade \n Current level: " + Integer.toString(level));
+                b.setText("+" + Integer.toString(addition));
+                t3.setText("200 puntos");
+            }else if (level <= 9) {
+                costemejora = 500;
+                if (Integer.parseInt(counter.getText().toString()) >= costemejora) {
+                    level++;
+                    b.setEnabled(true);
+                    counter.setTextColor(Color.rgb(0, 0, 0));
+                    counter.setText("" + (Integer.parseInt(counter.getText().toString()) - costemejora));
+                    addition *= addition;
+                    mikusais.setText("Tap me to upgrade \n Current level: " + Integer.toString(level));
+                    b.setText("+" + Integer.toString(addition));
+                    t3.setText("500 puntos");
+                }
+            }
         }
-        else{
-
-        }
-
-
     }
-
 }
