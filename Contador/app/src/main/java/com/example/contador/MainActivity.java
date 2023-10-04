0595872101c7feadbff9679f97cd92833a227960
dag45684 <dag45684@educastur.es>
2023-10-04 21:21:12 +0200

    threads y bigintegers

--- Contador/app/src/main/java/com/example/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/contador/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
     int addition = 100000;
     int level = 1;
     int costemejora = 100;
-    int coins=9999;
+    BigInteger coins = new BigInteger("9999");
+    BigInteger autoSumValue = new BigInteger("1");
     ScaleAnimation fade_in = new ScaleAnimation(0.7f, 1.2f, 0.7f, 1.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
 
     @Override
@@ -34,36 +39,56 @@ public class MainActivity extends AppCompatActivity {
         t3 = (TextView) findViewById(R.id.t3);
         lup = (LinearLayout) findViewById(R.id.lup);
         fade_in.setDuration(100);
+        autoSum();
     }
 
     public void sum(View v){
         lup.startAnimation(fade_in);
         counter.setTextColor(Color.rgb(0,0,0));
-        coins += addition;
-        if(coins>=1000 && coins<1000000){
-            counter.setText(Double.toString(coins/1000d).substring(0,3) + "Mil");
-        } else if (coins >= 1000000 && coins<1000000000) {
-            counter.setText(Double.toString(coins/1000000d).substring(0,4) + "M");
-        } else if (coins >= 1000000000) {
-            counter.setText(Double.toString(coins/1000000000d).substring(0,4) + "B");
+        coins.add(new BigInteger(Integer.toString(addition)));
+        if(coins.compareTo(new BigInteger("1000")) == 1 && coins.compareTo(new BigInteger("1000000")) != 1){
+//            counter.setText(Double.toString(coins/1000d).substring(0,3) + "Tho");
+            //TODO: THIS DOESNT WORK. JUST IMPLEMENT A FUCKING STRING ADDITION FOR THE COMMA THEN TRIM THE UNDESIRED SHIT.
+            counter.setText(new BigDecimal(coins.divide(new BigInteger("1000"))).toString());
+        } else if (coins.compareTo(new BigInteger("1000000")) == 1 && coins.compareTo(new BigInteger("1000000000")) != 1) {
+//            counter.setText(Double.toString(coins/1000000d).substring(0,4) + "M");
+            counter.setText(new BigDecimal(coins.divide(new BigInteger("1000000"))).toString());
+        } else if (coins.compareTo(new BigInteger("1000000000")) == 1 && coins.compareTo(new BigInteger("1000000000000")) != 1) {
+//            counter.setText(Double.toString(coins/1000000000d).substring(0,4) + "B");
+            counter.setText(new BigDecimal(coins.divide(new BigInteger("1000000000"))).toString());
+        } else if (coins.compareTo(new BigInteger("1000000000000")) == 1 && coins.compareTo(new BigInteger("1000000000000000")) != 1) {
+//            counter.setText(Double.toString(coins/1000000000d).substring(0,4) + "T");
+            counter.setText(new BigDecimal(coins.divide(new BigInteger("1000000000000"))).toString());
         }else{
-            counter.setText(Integer.toString(coins));
+            counter.setText(coins.toString());
         }
-        if(coins < costemejora){
+        if(coins.compareTo(new BigInteger(Integer.toString(costemejora))) == -1){
             lup.setEnabled(false);
             lup.setBackgroundColor(Color.rgb(220,220,220));
         }else{
             lup.setEnabled(true);
-            lup.setBackgroundColor(Color.rgb(255,200,180));
-            for (int i=2; i<=((int)coins/2)+1; i++){
-                if (coins%i == 0 && coins!=2){
-                    counter.setTextColor(Color.rgb(0,0,0));
-                    break;
-                }else counter.setTextColor(Color.rgb(255,0,0));
-            }
+//            lup.setBackgroundColor(Color.rgb(255,200,180));
+//            for (int i=2; i<=((int)coins/2)+1; i++){
+//                if (coins%i == 0 && coins!=2){
+//                    counter.setTextColor(Color.rgb(0,0,0));
+//                    break;
+//                }else counter.setTextColor(Color.rgb(255,0,0));
+//            }
         }
     }
 
+    public void autoSum (){
+        new Thread(() -> {
+            while(true){
+                coins = coins.add(autoSumValue);
+                runOnUiThread(() -> counter.setText(coins.toString()));
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+            }
+        }).start();
+    }
+    
     public void levelup(View v) {
         if (level <= 3) {
             if (Integer.parseInt(counter.getText().toString()) >= costemejora) {
