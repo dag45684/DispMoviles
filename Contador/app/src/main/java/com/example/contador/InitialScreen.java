0129354d459edfbcd5c6557e4e388da81ee560c3
dag45684 <dag45684@educastur.es>
2023-10-10 21:41:15 +0200

    initial screen adv

--- Contador/app/src/main/java/com/example/contador/InitialScreen.java
+++ Contador/app/src/main/java/com/example/contador/InitialScreen.java
@@ -2,7 +2,9 @@ package com.example.contador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InitialScreen extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class InitialScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_screen);
+    }
+
+    public void gotoMainActivity(View v){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
