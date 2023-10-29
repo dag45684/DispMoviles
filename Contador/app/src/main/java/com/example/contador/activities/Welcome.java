f3f6394d8fdd813533461b5fb89db915cce42fb3
Carlos <dag45684@educastur.es>
2023-10-29 13:31:13 +0100

    dialog

--- Contador/app/src/main/java/com/example/contador/activities/Welcome.java
+++ Contador/app/src/main/java/com/example/contador/activities/Welcome.java
@@ -1,8 +1,11 @@
 package com.example.contador.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -26,6 +29,23 @@ public class Welcome extends AppCompatActivity {
     public void gotoInfo(View v){
         Intent i = new Intent(this, About.class);
         startActivity(i);
+    }
+
+    public void popSettings(View v) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage("Settings activity is under development.")
+                .setIcon(R.drawable.settings)
+                .setNeutralButton("My Github", (_x, _y) -> {
+                    Intent b = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/cmgsk"));
+                    startActivity(b);
+                })
+                .setNegativeButton("Quit", (_x, _y) -> finish())
+                .setPositiveButton("Swap mode", (_x, _y) -> {
+                    if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO){
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    } else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                })
+                .show();
     }
     public void gotoSettings(View v){
         Intent i = new Intent(this, Settings.class);
